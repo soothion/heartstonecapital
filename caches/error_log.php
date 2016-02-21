5a1c1facf2198782c38f3147674c7798a55b91fd
Vincent <soothion@sina.com>
2016-02-21 18:31:47 +0800

    init

---
+++ caches/error_log.php
@@ -0,0 +1,3 @@
+<?php exit;?>02-21 18:04:53 | 2 | mysqli_connect(): (HY000/2002): No such file or directory | /Users/Vincent/www/heartstonecapital/install/install.php | 353
+<?php exit;?>02-21 18:05:00 | 2 | mysqli_connect(): (HY000/2002): Connection refused | /Users/Vincent/www/heartstonecapital/install/install.php | 353
+<?php exit;?>02-21 18:28:45 | 2 | chmod(): Operation not permitted | phpcms/modules/content/classes/html.class.php | 364
---
+++ phpsso_server/caches/caches_admin/caches_data/applist.cache.php
@@ -0,0 +1,16 @@
+<?php
+return array (
+  1 => 
+  array (
+    'appid' => '1',
+    'type' => 'phpcms_v9',
+    'name' => 'phpcms v9',
+    'url' => 'http://www.heartstonecapital.com/',
+    'authkey' => 'n6FHkxP0mND1wiZ5iG4soDBgDey5Agf8',
+    'ip' => '',
+    'apifilename' => 'api.php?op=phpsso',
+    'charset' => 'utf-8',
+    'synlogin' => '1',
+  ),
+);
+?>
